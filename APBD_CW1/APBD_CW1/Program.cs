96ec5f791eb14771a92ab5b8ec143f27fffaf043
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-03-02 09:38:50 +0100

    modyfikacja 4

--- APBD_CW1/APBD_CW1/Program.cs
+++ APBD_CW1/APBD_CW1/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+Console.WriteLine("Modyfikacja 4");
